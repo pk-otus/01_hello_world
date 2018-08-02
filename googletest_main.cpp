3570100c4840979e8d82a4d29a33c5750570b900
Paul <a@a.a>
2018-08-02 16:47:18 +0300

    +

---
+++ googletest_main.cpp
@@ -0,0 +1,14 @@
+#include "lib.h"
+
+#include "gtest/gtest.h"
+
+TEST(GoogleTestHW, gt_version_valid)
+{
+	ASSERT_GT(version(), 0);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
