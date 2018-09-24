02bcd8331278670ae1749593d29e6442378b5e8f
Paul <a@a.a>
2018-09-24 11:58:20 +0300

    add readme

--- google_test/googletest_main.cpp
+++ google_test/googletest_main.cpp
@@ -6,9 +6,3 @@ TEST(GoogleTestHW, gt_version_valid)
 {
 	ASSERT_GT(VersionControl::GetVersion(), 0);
 }
-
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
